3222e4ac630928717a7a4d4032b39b03bd13129b
Disk-MTH <gillet.mat@free.fr>
2025-07-16 19:08:55 +0200

    WIP fire effect

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,25 @@
 #include <Adafruit_NeoPixel.h>
 
 // --- Strips configuration ---
-#define LED_TYPE      (NEO_GRB + NEO_KHZ800)
-#define STRIPS_COUNT  7
-#define STRIP_1       70
-#define STRIP_2       62
-#define STRIP_3       46
-#define STRIP_4       64
-#define STRIP_5       28
-#define STRIP_6       52
-#define STRIP_7       50
+#define LED_TYPE (NEO_GRB + NEO_KHZ800)
+#define STRIPS_COUNT 7
+#define STRIP_1 70
+#define STRIP_2 62
+#define STRIP_3 46
+#define STRIP_4 64
+#define STRIP_5 28
+#define STRIP_6 52
+#define STRIP_7 50
 
 // --- Fire effect configuration ---
-#define RED           0xFF0000
-#define VIOLET        0xFF0000
-#define FLAME_SIZE    15
-#define COOLING       50
-#define SPARKING      100
-#define ANIMATION_DELAY   50
+#define BASE_COLOR 0xFF0000
+#define TIP_COLOR 0x280059
+
+#define FLAME_SIZE 10 // Flame size in pixels (0 - shortest led strip concerned by the effect)
+#define FLAME_TIP_SIZE 3 // Flame tip size in pixels (0 - FLAME_SIZE)
+#define COOLING 50 // Cooling factor (0 - no cooling, 255 - maximum cooling)
+#define SPARKING 50 // Spark chance (0 - no sparks, 255 - maximum sparks)
+#define ANIMATION_DELAY 125 // Animation delay in milliseconds
 
 // --- Strips configuration ---
 Adafruit_NeoPixel left_strips[STRIPS_COUNT] = {
@@ -42,9 +44,8 @@ Adafruit_NeoPixel right_strips[STRIPS_COUNT] = {
 
 // --- Global variables for fire effect ---
 uint8_t MASTER_BRIGHTNESS = 150;
-uint8_t MIN_FLAME_BRIGHTNESS = MASTER_BRIGHTNESS * 0.6;
-byte heat_left[STRIPS_COUNT][FLAME_SIZE];
-byte heat_right[STRIPS_COUNT][FLAME_SIZE];
+uint8_t MIN_FLAME_BRIGHTNESS = MASTER_BRIGHTNESS * 0.2;
+uint8_t heat[STRIPS_COUNT][FLAME_SIZE];
 uint32_t flame_gradient[FLAME_SIZE];
 
 // --- Utility functions ---
@@ -68,22 +69,26 @@ uint8_t random8() {
     return random(256);
 }
 
-void animateFire(Adafruit_NeoPixel &strip, byte heat[FLAME_SIZE]) {
-    int numPixels = strip.numPixels();
+void animateFire(Adafruit_NeoPixel &strip, uint8_t heat[FLAME_SIZE]) {
+    for (uint8_t i = 0; i < FLAME_SIZE; i++) {
+        heat[i] = qsub8(heat[i], random8(0, COOLING * 10 / FLAME_SIZE + 2));
+    }
 
-    for (int i = 0; i < FLAME_SIZE; i++) { heat[i] = qsub8(heat[i], random8(0, ((COOLING * 10) / FLAME_SIZE) + 2)); }
-    for (int k = FLAME_SIZE - 1; k >= 2; k--) { heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2]) / 3; }
+    for (uint8_t i = FLAME_SIZE - 1; i >= 2; i--) {
+        heat[i] = (heat[i - 1] + heat[i - 2] + heat[i - 2]) / 3;
+    }
+
     if (random8() < SPARKING) {
-        int y = random8(0, 7);
+        const uint8_t y = random8(0, 3);
         heat[y] = qadd8(heat[y], random8(160, 255));
     }
 
     for (int j = 0; j < FLAME_SIZE; j++) {
         const uint32_t base_color = flame_gradient[j];
-        const byte pixel_brightness = heat[j] == 0 ? 0 : map(heat[j], 0, 255, MIN_FLAME_BRIGHTNESS, 255);
-        
+        const uint8_t pixel_brightness = heat[j] == 0 ? 0 : map(heat[j], 0, 255, MIN_FLAME_BRIGHTNESS, 255);
+
         strip.setPixelColor(
-            numPixels - FLAME_SIZE + j,
+            strip.numPixels() - FLAME_SIZE + j,
             (base_color >> 16 & 0xFF) * pixel_brightness / 255,
             (base_color >> 8 & 0xFF) * pixel_brightness / 255,
             (base_color & 0xFF) * pixel_brightness / 255
@@ -95,21 +100,21 @@ void animateFire(Adafruit_NeoPixel &strip, byte heat[FLAME_SIZE]) {
 void setup() {
     Serial.begin(115200);
     randomSeed(analogRead(0));
-    
-    for (int i = 0; i < FLAME_SIZE; i++) {
-        flame_gradient[i] = Adafruit_NeoPixel::Color(
-            map(i, 0, FLAME_SIZE - 1, RED >> 16 & 0xFF, VIOLET >> 16 & 0xFF),
-            map(i, 0, FLAME_SIZE - 1, RED >> 8 & 0xFF, VIOLET >> 8 & 0xFF),
-            map(i, 0, FLAME_SIZE - 1, RED & 0xFF, VIOLET & 0xFF)
-        );
+
+    for (uint8_t i = 0; i < FLAME_SIZE; i++) {
+        flame_gradient[i] = BASE_COLOR;
+    }
+
+    for (uint8_t i = FLAME_SIZE - FLAME_TIP_SIZE; i < FLAME_SIZE; i++) {
+        flame_gradient[i] = TIP_COLOR;
     }
 
     for (uint8_t i = 0; i < STRIPS_COUNT; i++) {
         left_strips[i].begin();
-        left_strips[i].fill(RED);
+        left_strips[i].fill(BASE_COLOR);
 
         right_strips[i].begin();
-        right_strips[i].fill(RED);
+        right_strips[i].fill(BASE_COLOR);
     }
 }
 
@@ -118,19 +123,19 @@ void loop() {
 
     for (uint8_t i = 0; i < STRIPS_COUNT; i++) {
         left_strips[i].setBrightness(MASTER_BRIGHTNESS);
-        
+
         right_strips[i].setBrightness(MASTER_BRIGHTNESS);
     }
 
     for (const uint8_t i: {1, 3, 5, 6}) {
-        animateFire(left_strips[i], heat_left[i]);
-        
-        animateFire(right_strips[i], heat_right[i]);
+        animateFire(left_strips[i], heat[i]);
+
+        animateFire(right_strips[i], heat[i]);
     }
 
     for (uint8_t i = 0; i < STRIPS_COUNT; i++) {
         left_strips[i].show();
-        
+
         right_strips[i].show();
     }
 
